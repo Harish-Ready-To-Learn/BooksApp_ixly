e85c9f70bbec5afac600bd04f470f7e88348947a
Harish <vharish@quessgts.comv>
2025-03-19 13:38:24 +0530

    book details screen and redux

--- App.jsx
+++ App.jsx
@@ -2,12 +2,16 @@ import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import RootNavigation from './src/navigation/RootNavigation';
 import {NavigationContainer} from '@react-navigation/native';
+import {Provider} from 'react-redux';
+import store from './src/store/redux/store';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <RootNavigation />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <RootNavigation />
+      </NavigationContainer>
+    </Provider>
   );
 };
 
